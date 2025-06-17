8eedcf1d7352752870ae235ff422235b272d3d80
Beatriz Abreu <beahabreu@gmail.com>
2025-06-16 21:29:14 -0300

    atualizações

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import VideoGrid from './VideoGrid';
 function App() {
   return (
     <div style={{ backgroundColor: '#000', height: '100vh' }}>
-      <h1 style={{ color: '#fff', padding: '1rem', textAlign: 'center' }}>NaviView APP</h1>
+      <h1 style={{ color: '#ffdb58', padding: '1rem', textAlign: 'center', fontSize: '64px', textShadow: '2px 2px 4px #fff'}}>NaviView APP</h1>
       <VideoGrid />
     </div>
   );
